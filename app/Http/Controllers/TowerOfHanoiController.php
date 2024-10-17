dad31edaee94f3e554c4892557920b34c11f1852
infinitypaul <infinitypaul@live.com>
2024-10-17 19:17:22 +0100

    Refactor TowerOfHanoiController to use TowerOfHanoiService

--- app/Http/Controllers/TowerOfHanoiController.php
+++ app/Http/Controllers/TowerOfHanoiController.php
@@ -2,73 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\TowerOfHanoiService;
 use Illuminate\Http\Request;
 
 class TowerOfHanoiController extends Controller
 {
-
-    private array $pegs;
-    private int $totalDisks = 7;
-
-    private bool $gameOver = false;
-    public function __construct()
+    public function __construct(public TowerOfHanoiService $hanoiService)
     {
-        $this->pegs = [
-            range($this->totalDisks, 1),
-            [],
-            []
-        ];
     }
 
     public function getState()
     {
-        return response()->json([
-            'pegs' => $this->pegs,
-            'gameOver' => $this->gameOver
-        ]);
+        return response()->json(
+            $this->hanoiService->fetchGameState()
+        );
+
     }
 
     public function move($from, $to)
     {
-        if($this->gameOver) {
-            return response()->json([
-                'message' => 'Game Over'
-            ], 400);
+        $result = $this->hanoiService->move($from, $to);
+
+        if ($result === TowerOfHanoiService::GAME_OVER || $result === TowerOfHanoiService::INVALID_MOVE) {
+            return response()->json(['message' => $result], 400);
         }
 
-        $source = $from - 1;
-        $destination = $to - 1;
+        return response()->json($this->hanoiService->fetchGameState());
 
-        if(!$this->isValidMove($source, $destination)) {
-            return response()->json([
-                'message' => 'Invalid Move'
-            ], 400);
-        }
-
-        $disk = array_pop($this->pegs[$source]);
-        $this->pegs[$destination][] = $disk;
-
-        if(count($this->pegs[2]) === $this->totalDisks) {
-            $this->gameOver = true;
-            return response()->json([
-                'message' => 'Game Over'
-            ], 200);
-        }
-
-        return  $this->getState();
-
-    }
-
-    private function isValidMove($source, $destination)
-    {
-        if(empty($this->pegs[$source])) {
-            return false;
-        }
-
-        if(empty($this->pegs[$destination])) {
-            return true;
-        }
-
-        return end($this->pegs[$source]) < end($this->pegs[$destination]);
     }
 }

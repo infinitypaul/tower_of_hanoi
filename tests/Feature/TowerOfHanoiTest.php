9673da63a84a4e69ccfedba7ef3b4f0e75ee3143
infinitypaul <infinitypaul@live.com>
2024-10-17 14:26:10 +0100

    Add TowerOfHanoi feature test

---
+++ tests/Feature/TowerOfHanoiTest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Tests\Feature;
+
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Illuminate\Foundation\Testing\WithFaker;
+use Tests\TestCase;
+
+class TowerOfHanoiTest extends TestCase
+{
+    /**
+     * A basic feature test example.
+     */
+    public function testValidMove(): void
+    {
+        $response = $this->postJson('/api/move/1/2');
+
+        $response->assertStatus(200);
+
+        $response->assertJson([
+            'pegs' => [
+                [7, 6, 5, 4, 3, 2],
+                [1],
+                []
+            ],
+            'gameOver' => false
+        ]);
+    }
+}
